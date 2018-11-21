49f674e6335108fa41ccb89e0adff1292ef23b44
YannisNaidja <yannis.naidja@etu.univ-amu.fr>
2018-11-21 16:59:05 +0100

    Interface graphique

--- src/java/utils/Enveloppe.java
+++ src/java/utils/Enveloppe.java
@@ -5,31 +5,38 @@ import javafx.geometry.Point2D;
 import java.awt.*;
 
 public class Enveloppe {
-
     boolean aretes[][];
+
 
     public Enveloppe(int nbPoints) {
         aretes = new boolean[nbPoints][nbPoints];
         for(int i=0;i<nbPoints;i++)
             for(int j=0;j<nbPoints;j++)
                 aretes[i][j] = false;
-            aretes[1][4] = true;
     }
 
     public void CreerEnveloppe(Point2D[] nuage){
+        int nbPoints = nuage.length;
+
+
+
         if(nuage.length > 3){
             int milieu = nuage.length/2;
+
             Point2D[] sousNuage1 = new Point2D[milieu];
-            Point2D[] sousNuage2 = new Point2D[nuage.length-milieu];
             for(int i=0;i<milieu;i++)
                 sousNuage1[i] = nuage[i];
+
+            Point2D[] sousNuage2 = new Point2D[nuage.length-milieu];
             for(int i=milieu;i<nuage.length;i++)
                 sousNuage2[i] = nuage[i];
+
             CreerEnveloppe(sousNuage1);
             CreerEnveloppe(sousNuage2);
         }
 
-
-
+        if(nuage.length == 2){
+            aretes[0][1] = true;
+        }
     }
 }
